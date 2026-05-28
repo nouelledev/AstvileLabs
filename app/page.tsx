3da1731977487364e84c4a8cc4c50fd0c51cb11a
Nouelle <ncprs10@gmail.com>
2026-05-28 22:28:55 +0800

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,14 @@ import { SocialProofStrip } from "./components/social-proof-strip";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen overflow-x-hidden bg-[#101010] text-white">
-      <div className="pointer-events-none fixed inset-0 z-50 opacity-[0.035] [background-image:radial-gradient(circle_at_1px_1px,white_1px,transparent_0)] [background-size:4px_4px]" />
+    <>
       <Navbar />
-      <div className="h-16" aria-hidden="true" />
       <HeroSection />
       <SocialProofStrip />
       <ProcessSection />
       <RatesSection />
-      <ContactSection />
       <FAQSection />
-    </main>
+      <ContactSection />
+    </>
   );
 }
